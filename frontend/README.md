744d0431b8a56e2fd4bbbdc44f402fa1ac3b1bd0
Eric Zhou <ericzhou03@Erics-MacBook-Pro.local>
2025-02-07 17:26:59 -0500

    First Commit with Frontend Template

---
+++ frontend/README.md
@@ -0,0 +1,7 @@
+Project for app
+
+To use, copy the files over
+Then run ```npm install next react react-dom```
+Then run ```npm audit fix```
+Then run ```npm run build```
+Then run ```npm run dev```
---
+++ frontend/src/pages/index.js
@@ -0,0 +1,16 @@
+import { Box } from "@chakra-ui/react";
+
+export default function Home() {
+  return (
+    <Box 
+      width="100vw" 
+      height="100vh" 
+      backgroundColor="white" 
+      display="flex" 
+      justifyContent="center" 
+      alignItems="center"
+    >
+      Hello, World!
+    </Box>
+  );
+}
